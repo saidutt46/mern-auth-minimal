3254c5a79b04148a15f47697cf8ea024009f68e9
Ganduri Venkata Sai Dutt <duttganduri46@gmail.com>
2021-04-20 00:50:50 -0400

    prod support for heroku deps

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const path = require('path');
 
 const app = express();
 
@@ -30,6 +31,15 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+
+// serve static assets if in prod mode
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Hurray, Server up and running on port ${port} !`));
