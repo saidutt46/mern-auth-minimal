fa3d24170d06eb5d487c6e1ba84a58ca5e607821
Ganduri Venkata Sai Dutt <duttganduri46@gmail.com>
2021-04-24 22:17:11 -0400

    updated code format, heroku err fix tk1

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: false,
   })
 );
 app.use(bodyParser.json());
@@ -18,12 +18,18 @@ app.use(bodyParser.json());
 const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
+
+// serve static assets if in prod mode
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Passport middleware
 app.use(passport.initialize());
@@ -32,14 +38,7 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-// serve static assets if in prod mode
-if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`Hurray, Server up and running on port ${port} !`));
+app.listen(port, () =>
+  console.log(`Hurray, Server up and running on port ${port} !`)
+);
